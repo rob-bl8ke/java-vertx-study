63dfc44e02c6d1294dbadf8f948e5d44333b1626
Rob Blake <rob.bl8ke@outlook.com>
2025-03-05 13:22:29 +0200

    Refactor to private handler methods

--- src/main/java/com/cyg/demo/MainVerticle.java
+++ src/main/java/com/cyg/demo/MainVerticle.java
@@ -2,6 +2,7 @@ package com.cyg.demo;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -10,18 +11,22 @@ public class MainVerticle extends AbstractVerticle {
         Router router = Router.router(vertx);
 
         // GET without a path parameter
-        router.get("/api/v1/hello").handler(ctx -> {
-            ctx.request().response().end("Hello Vert.x World!");
-        });
+        router.get("/api/v1/hello").handler(this::helloVertx);
 
         // GET with a path parameter
-        router.get("/api/v1/hello/:name").handler(ctx -> {
-            String name = ctx.pathParam("name");
-            ctx.request().response().end(String.format("Hello %s!", name));
-        });
+        router.get("/api/v1/hello/:name").handler(this::helloName);
 
         vertx.createHttpServer()
                 .requestHandler(router)
                 .listen(8080);
+    }
+
+    void helloVertx(RoutingContext ctx) {
+        ctx.request().response().end("Hello Vert.x World!");
+    }
+
+    void helloName(RoutingContext ctx) {
+        String name = ctx.pathParam("name");
+        ctx.request().response().end(String.format("Hello %s!", name));
     }
 }
